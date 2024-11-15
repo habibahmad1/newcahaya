c4d374fde039625917c5f5518698f73a506abcf4
Habib Ahmad <habibahmad2597@gmail.com>
2024-11-15 08:55:05 +0700

    tambah faq

--- js/script.js
+++ js/script.js
@@ -107,3 +107,17 @@ document.addEventListener("DOMContentLoaded", () => {
     },
   });
 });
+
+// FAQ
+// JavaScript untuk menambahkan interaksi klik pada setiap card FAQ
+document.querySelectorAll(".card-faq").forEach((card) => {
+  card.addEventListener("click", () => {
+    // Hapus kelas 'active' dari semua kartu lainnya
+    document
+      .querySelectorAll(".card-faq")
+      .forEach((c) => c.classList.remove("active"));
+
+    // Tambahkan kelas 'active' ke kartu yang diklik
+    card.classList.toggle("active");
+  });
+});
